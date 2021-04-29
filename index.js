c1802a3af653aacca45fcdc9d1412fcce5877790
Luís Ângelo Rodrigues Jr <luisangelorjr@gmail.com>
2021-04-29 16:07:47 -0300

    Only Indentation

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function myFunc(arg){
     console.log(`arg was => ${arg}`);
-  }
+}
   
-  console.log("Wait 1 minute for next console.log()");
-  setTimeout(myFunc, 1 * 1000 * 60 , 'funky');
+console.log("Wait 1 minute for next console.log()");
+setTimeout(myFunc, 1 * 1000 * 60 , 'funky');
   
